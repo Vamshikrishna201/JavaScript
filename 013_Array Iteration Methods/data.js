1f7cfc1b7e4127181f9af8f2e2ec6d2d32783bc6
Vamshikrishna <rvkrishna21@gmail.com>
2025-12-21 00:02:37 +0530

    Product data file in Object of arrays

---
+++ 013_Array Iteration Methods/data.js
@@ -0,0 +1,131 @@
+export const products = [
+  {
+    id: 1,
+    name: "Action Camera",
+    category: "Electronics",
+    price: 16200,
+    inStock: true,
+  },
+
+  {
+    id: 2,
+    name: "Portable Charger",
+    category: "Electronics",
+    price: 2000,
+    inStock: true,
+  },
+
+  {
+    id: 3,
+    name: "Smart Light Bulb (2-pack)",
+    category: "Electronics",
+    price: 2200,
+    inStock: true,
+  },
+
+  {
+    id: 4,
+    name: "External Hard Drive (2TB)",
+    category: "Electronics",
+    price: 5400,
+    inStock: true,
+  },
+
+  {
+    id: 5,
+    name: "Running Shorts",
+    category: "Apparel",
+    price: 2100,
+    inStock: true,
+  },
+
+  {
+    id: 6,
+    name: "Formal Shoes (Leather)",
+    category: "Apparel",
+    price: 6200,
+    inStock: true,
+  },
+
+  {
+    id: 7,
+    name: "Rain Jacket",
+    category: "Apparel",
+    price: 4200,
+    inStock: false,
+  },
+
+  {
+    id: 8,
+    name: "Microwave Oven",
+    category: "Kitchenware",
+    price: 6300,
+    inStock: true,
+  },
+
+  {
+    id: 9,
+    name: "Cutting Board (Wood)",
+    category: "Kitchenware",
+    price: 1150,
+    inStock: true,
+  },
+
+  {
+    id: 10,
+    name: "Whiteboard Markers (4-pack)",
+    category: "Office Supplies",
+    price: 550,
+    inStock: true,
+  },
+
+  {
+    id: 11,
+    name: "Desk Organizer",
+    category: "Office Supplies",
+    price: 1650,
+    inStock: true,
+  },
+
+  {
+    id: 12,
+    name: "Mystery Novel",
+    category: "Books",
+    price: 1250,
+    inStock: true,
+  },
+
+  {
+    id: 13,
+    name: "Self-Help Book",
+    category: "Books",
+    price: 1600,
+    inStock: true,
+  },
+
+  {
+    id: 14,
+    name: "Resistance Bands Set",
+    category: "Fitness",
+    price: 2500,
+    inStock: true,
+  },
+
+  { id: 15, name: "Jump Rope", category: "Fitness", price: 650, inStock: true },
+
+  {
+    id: 16,
+    name: "Garden Gloves",
+    category: "Gardening",
+    price: 800,
+    inStock: true,
+  },
+
+  {
+    id: 17,
+    name: "Plant Pot (Medium)",
+    category: "Gardening",
+    price: 1000,
+    inStock: true,
+  },
+];
